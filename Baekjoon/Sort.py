5ff6edcded67ca2af8252c9a592e093f5f7f5d8b
DESKTOP-60TM4II\KimHeeSu <wlskaka4@gmail.com>
2021-01-21 11:11:02 +0900

    Baekjoon sort 2108

--- Baekjoon/Sort.py
+++ Baekjoon/Sort.py
@@ -1,13 +1,29 @@
 import sys
-n = int(input())
-check_list = [0] * (n+1)
+from collections import Counter
 
-for i in range(n):
-    input_num = int(sys.stdin.readline())
-    check_list[input_num] = check_list[input_num] + 1
+n = int(sys.stdin.readline())
+arr = []
+for _ in range(n):
+    arr.append(int(sys.stdin.readline()))
 
-for i in range(10001):
-    if check_list[i] != 0:
-        for j in range(check_list[i]):
-            print(i)
+print(sum(arr)//len(arr))
+def median(arr):
+    arr.sort()
+    return arr[len(arr)//2]
+print(median(arr))
 
+def mode(arr):
+
+    mode = Counter(arr).most_common()
+
+    if len(arr) > 1:
+        if mode[0][1] == mode[1][1]:
+            return mode[1][0]
+        else:
+            return mode[0][0]
+    else:
+        return mode[0][0]
+
+
+print(mode(arr))
+print(max(arr)-min(arr))
