ec174f2c572cde3e2dd9fac9535f4dc55b352e56
DESKTOP-60TM4II\KimHeeSu <wlskaka4@gmail.com>
2021-01-23 19:39:29 +0900

    no message

--- Baekjoon/Sort.py
+++ Baekjoon/Sort.py
@@ -1,29 +1,10 @@
-import sys
-from collections import Counter
+import hashlib
+import os
+f = open("C:\Program Files\Docker\Docker\InstallerCli.exe", 'rb')
+data = f.read()
+f.close()
 
-n = int(sys.stdin.readline())
-arr = []
-for _ in range(n):
-    arr.append(int(sys.stdin.readline()))
-
-print(sum(arr)//len(arr))
-def median(arr):
-    arr.sort()
-    return arr[len(arr)//2]
-print(median(arr))
-
-def mode(arr):
-
-    mode = Counter(arr).most_common()
-
-    if len(arr) > 1:
-        if mode[0][1] == mode[1][1]:
-            return mode[1][0]
-        else:
-            return mode[0][0]
-    else:
-        return mode[0][0]
-
-
-print(mode(arr))
-print(max(arr)-min(arr))
+print("CRC32: "+hashlib.)
+print("MD5: " + hashlib.md5(data).hexdigest().upper())
+print("SHA-1: " + hashlib.sha1(data).hexdigest().upper())
+print("SHA-256: " + hashlib.sha256(data).hexdigest().upper())
